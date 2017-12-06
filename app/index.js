8fc9a1075ccfbcaa75b8a84de4633b19f583607d
Finleth <jeffrey@paufamily.com>
2017-12-06 12:55:21 -0800

    made new calc.js file and added math functions

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import * as math from './calc';
+
+console.log('5 + 7 = ', math.default(5, 7));
+
+console.log('90 - 16 = ', math.subtract(90, 16));
+
+console.log('7 x 100 = ', math.multiply(7, 100));
+
