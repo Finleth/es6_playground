36377166064941ea0c7b4379745ce40f7bf5b798
Finleth <jeffrey@paufamily.com>
2017-12-06 15:18:59 -0800

    added person.js that has person class and super person class with a few greeting methods

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import SuperPerson from './person';
+
+const tomato = new SuperPerson('Tomato', 20);
+
+console.log("Tomato: ", tomato.sayHello() );
+console.log("Tomato: ", tomato.superSayHi() );
+
